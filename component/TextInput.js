595e578cc89ce9c2f3eb4b9d2bf6740b8f24461f
nimwei <254060001@qq.com>
2018-03-12 16:09:34 +0800

    Parcel打包入门

---
+++ component/TextInput.js
@@ -0,0 +1,21 @@
+/**
+ * Created by nimengwei on 2018/3/12.
+ */
+
+import React, { Component } from 'react'
+
+export default class TextInput extends Component {
+    constructor(props, context){
+        super(props, context);
+    }
+
+    componentDidMount(){
+        console.log("input did mount");
+    }
+
+    render() {
+        return(
+            <input type="text"  />
+        )
+    }
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by nimengwei on 2018/3/9.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TextInput from './component/TextInput'
+
+ReactDOM.render(
+    <TextInput />,
+    document.getElementById("App")
+);
